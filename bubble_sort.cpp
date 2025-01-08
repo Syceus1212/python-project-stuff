36244af2dd5fa28e2321408d874346216b14160a
Syceus1212 <notrandom20001@gmail.com>
2025-01-08 17:25:08 +0530

    Create bubble_sort.cpp

---
+++ bubble_sort.cpp
@@ -0,0 +1,36 @@
+// bubble sort 
+
+#include <iostream> 
+using namespace std;
+
+void sort(int array[], int size);
+
+int main() {
+    
+    int array[] = {1, 3, 4, 6, 8, 2, 7, 9, 10};
+    int size = sizeof(array)/sizeof(array[0]);
+
+    sort(array, size);
+
+    for(int element : array) {
+        cout << element << " ";
+    }
+
+    return 0;
+}
+
+void sort(int array[], int size) {
+
+    int temp;
+    
+    for(int i = 0; i < size - 1; i++) {
+        for(int j = 0; j < size - i - 1; j++){
+            if (array[j] > array[j+1]){
+                temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+
+}
